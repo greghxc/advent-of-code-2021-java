deb579bbc2be282463483d72f016bc956eedf08d
greghxc <greg@hacksy.io>
2021-12-01 20:04:51 -0800

    day one!

--- src/main/java/io/hacksy/aoc/v2021/day01/Day01Processor.java
+++ src/main/java/io/hacksy/aoc/v2021/day01/Day01Processor.java
@@ -1,13 +1,16 @@
 package io.hacksy.aoc.v2021.day01;
 
 import io.vavr.collection.List;
+import io.vavr.collection.Stream;
 
 public class Day01Processor {
     Integer partOne(List<Integer> input) {
-        return null;
+        return Stream.range(1, input.size())
+                .count(i -> input.get(i) > input.get(i - 1));
     }
 
     Integer partTwo(List<Integer> input) {
-        return null;
+        return Stream.range(3, input.size())
+                .count(i -> input.get(i) > input.get(i - 3));
     }
 }
